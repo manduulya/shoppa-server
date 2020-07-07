aab2db9f632558a1ddafc386251f4dd7be7751f3
Beyonce Ebo <manduulya@yahoo.com>
2020-07-06 23:24:03 -0400

    initial

---
+++ src/dummyStore.js
@@ -0,0 +1,30 @@
+module.exports = {
+  user: [
+    {
+      id: 1,
+      Name: "Mynameis",
+      password: "mypassword",
+    },
+  ],
+  shoppingList: [
+    {
+      id: 1,
+      user_id: 1,
+      name: "myList",
+      store_id: 1,
+    },
+  ],
+  store: [
+    {
+      id: 1,
+      name: "target",
+    },
+  ],
+  item: [
+    {
+      id: 1,
+      name: "apple",
+      store_id: 1,
+    },
+  ],
+};
---
+++ src/stores/stores-router.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const { store } = require("../dummyStore");
+const storeRouter = express.Router();
+const bodyParser = express.json();
+const dummyStore = require("../dummyStore");
+
+storeRouter
+  .route("/stores")
+  .get((req, res) => {
+    res.json(dummyStore.store);
+  })
+  .post(bodyParser, (req, res) => {
+    const { id, name } = req.body;
+    const newStore = { id, name };
+
+    if (!name) {
+      res
+        .status(400)
+        .json({ error: { message: `Missing store name in the request body` } });
+    }
+    store.push(newStore);
+    res.status(201).json(newStore);
+  });
+storeRouter
+  .route("/stores/:id")
+  .get((req, res) => {
+    const { id } = req.params;
+    const st = dummyStore.store.find((s) => s.id == id);
+    if (!id) {
+      return res.status(404).json({ error: { message: `Store not found` } });
+    }
+    res.json(st);
+  })
+  .delete((req, res) => {
+    const { deleteStoreId } = req.params;
+    const index = dummyStore.store.findIndex((s) => s.id === deleteStoreId);
+    console.log(index);
+
+    if (index === -1) {
+      return res.status(404).json({ error: { message: "Not Found" } });
+    }
+    dummyStore.store.splice(index, 1);
+    res.status(204).end();
+  });
+
+module.exports = storeRouter;
